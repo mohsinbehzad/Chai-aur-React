6a756abf9d3e36b2f1bd8c798cf79a13dce4451d
mohsinbehzad <mohsinbehzad14@gmail.com>
2025-07-20 11:26:38 +0500

    Random Password Generator UI added

---
+++ 05.Random-Password-Generator/src/App.jsx
@@ -0,0 +1,66 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [passwordLength, setPasswordLength] = useState(8)
+  const [includeNumbers, setIncludeNumbers] = useState(false)
+  const [includeSymbols, setIncludeSymbols] = useState(false)
+  const [generatedPassword, setGeneratedPassword] = useState("")
+
+  return (
+    <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-teal-300 text-violet-700'>
+      <h1 className='text-violet-700 my-3'>Password Generator</h1>
+      <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+        <input
+          type="text"
+          value={generatedPassword}
+          className='outline-none w-full py-1 px-3'
+          placeholder='Generated Password'
+          readOnly
+        />
+        <button
+          className='outline-none bg-violet-600 text-white px-3 py-0.5 shrink-0 hover:bg-teal-600'>
+          copy</button>
+      </div>
+
+      <div className='flex text-sm gap-x-2'>
+        <div className='flex items-center gap-x-1'>
+          <input
+            type="range"
+            min={8}
+            max={100}
+            value={passwordLength}
+            className='cursor-pointer'
+            onChange={(e) => { setPasswordLength(e.target.value) }}
+          />
+          <label>Length: {passwordLength}</label>
+        </div>
+
+        <div className='flex items-center gap-x-1'>
+          <input
+            type="checkbox"
+            defaultChecked={includeNumbers}
+            id="numberInput"
+            onChange={() => {
+              setIncludeNumbers((prev) => !prev);
+            }}
+          />
+          <label htmlFor="numberInput">Numbers</label>
+        </div>
+        <div className='flex items-center gap-x-1'>
+          <input
+            type="checkbox"
+            id='symbolInput'
+            defaultChecked={includeSymbols}
+            onChange={() => {
+              setIncludeSymbols((prev) => !prev)
+            }}
+          />
+          <label htmlFor="symbolInput">Symbols</label>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
