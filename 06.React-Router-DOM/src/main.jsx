dde777f9198e39d6b948055bfd702f0b6b7985ef
mohsinbehzad <mohsinbehzad14@gmail.com>
2025-08-14 06:18:04 +0500

    Modern Syntax for Routing

--- 06.React-Router-DOM/src/main.jsx
+++ 06.React-Router-DOM/src/main.jsx
@@ -1,34 +1,46 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Root from './Root'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
 
-const router = createBrowserRouter([
-  {
-    path: "",
-    element: <Root />,
-    children: [
-      {
-        path: "/",
-        element: <Home />
-      },
-      {
-        path: "/about",
-        element: <About />
-      },
-      {
-        path: "/contact",
-        element: <Contact />
-      },
-    ]
-  }
-])
+// const router = createBrowserRouter([
+//   {
+//     path: "",
+//     element: <Root />,
+//     children: [
+//       {
+//         path: "/",
+//         element: <Home />
+//       },
+//       {
+//         path: "/about",
+//         element: <About />
+//       },
+//       {
+//         path: "/contact",
+//         element: <Contact />
+//       },
+//     ]
+//   }
+// ])
+
+// Modern syntax for routing
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='' element={<Root />}>
+      <Route path='/' element={<Home />}></Route>
+      <Route path='/about' element={<About />}></Route>
+      <Route path='/contact' element={<Contact />}></Route>
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router= {router} />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
