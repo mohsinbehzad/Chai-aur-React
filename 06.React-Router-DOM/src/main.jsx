41da103312e5c65096efb962f9f68e446816df8c
mohsinbehzad <mohsinbehzad14@gmail.com>
2025-08-14 05:06:58 +0500

    createBrowserRouter, RouterProvider added

--- 06.React-Router-DOM/src/main.jsx
+++ 06.React-Router-DOM/src/main.jsx
@@ -1,9 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Root from './Root'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: "",
+    element: <Root />,
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Root />
+    <RouterProvider router= {router} />
   </StrictMode>,
 )
