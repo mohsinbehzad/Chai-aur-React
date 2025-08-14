932eb882dde06a21868f9b9be4ff5b1041cd2506
mohsinbehzad <mohsinbehzad14@gmail.com>
2025-08-14 05:06:36 +0500

    Outlet added

--- 06.React-Router-DOM/src/Root.jsx
+++ 06.React-Router-DOM/src/Root.jsx
@@ -1,9 +1,17 @@
 import "./index.css"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import { Outlet } from "react-router-dom"
 
 function Root() {
     return <>
-        <h1 className="bg-gray-700">React Router Dom</h1>
+        <Header />
+        <Outlet />  {/* Child routes will be rendered here */}
+        <Footer />
+
     </>
 }
 
 export default Root
+
+// <Outlet /> is a placeholder component that tells React Router where to render nested child routes inside a parent route component.
