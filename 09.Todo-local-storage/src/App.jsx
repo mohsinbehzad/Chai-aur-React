319bf874374e196c7b6ee738901c09d0a28ed8e0
mohsinbehzad <mohsinbehzad14@gmail.com>
2025-08-17 23:50:57 +0500

    TodoContext setup

---
+++ 09.Todo-local-storage/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import { TodoProvider } from './contexts/TodoContext'
+
+function App() {
+
+  return (
+    <TodoProvider>
+      
+    </TodoProvider>
+  )
+}
+
+export default App
